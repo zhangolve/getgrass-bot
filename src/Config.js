ccb8db0249a79a63809e48e752e046a90826fca9
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-03 16:44:06 +0800

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,10 @@
-class Configuration {
+class Config {
   constructor() {
-    this.ipCheckURL = 'https://ipinfo.io/json'; // IP检查URL
-    this.wssHost = 'proxy.your-domain.com:4444'; // WebSocket服务器地址
-    this.retryInterval = 20000; // 重试间隔（20秒）
+    this.ipCheckURL = 'https://ipinfo.io/json';
+    this.wssHost = 'proxy.wynd.network:4444';
+    this.retryInterval = 20000;
   }
 }
 
-module.exports = Configuration; // 导出Configuration类
+module.exports = Config;
 
