a631bcdde0bb8272348d2c8efc20b28ad3054e02
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-03 08:18:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-USER_ID = 'your_getgrass_user_id'
+# 配置文件
+
+用户_ID = '你的_getgrass用户_ID'
+USE_PROXY = True  # 设置为 True 使用代理，设置为 False 不使用代理
 HTTP_PROXY_LIST = [
-    'http://username:password@hostname:port',
-    # 如果需要，可以添加更多代理 URL
+    'http://用户名:密码@主机名:端口',
+    # 如果不使用代理，可以将这个列表留空
 ]
