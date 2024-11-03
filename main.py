a19ff22cdee898cb96b0f3381350d240eb85feab
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-03 08:19:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# 主入口文件
-
 import asyncio
 from connect_to_wss import 连接到_wss
-from config import HTTP_PROXY_LIST
+from config import HTTP_PROXY_LIST, USE_PROXY
 
 async def main():
-    任务 = [连接到_wss(代理) for 代理 in HTTP_PROXY_LIST]
-    await asyncio.gather(*任务)
+    if USE_PROXY and HTTP_PROXY_LIST:
+        tasks = [连接到_wss(proxy) for proxy in HTTP_PROXY_LIST]
+    else:
+        tasks = [连接到_wss()]  # 不使用代理时，直接连接
+    await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
     asyncio.run(main())
